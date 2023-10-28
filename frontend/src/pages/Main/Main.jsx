a470d3cae080aba1f43c7fc9d5563ce1fd46983f
ava-mirtl <ava_mirtl@mail.ru>
2023-10-28 18:38:42 +0300

    modal

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+import tlg from '../../assets/icons/tlg-icon.svg'
 import styles from './main.module.scss';
 
 function Main() {
-  let error={};
-  error.message='Такого аккаунта не существует';
+  const token = window.token; 
+  const [error, setError] = useState({}); 
+  const [popup, setPopup] = useState(false); 
+  let handleClick = (e)=>{
+    e.preventDefault();
+    if(token) {
+      
+    }else{
+      setPopup(true);
+    }
+  }
+  const handlePopupClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setPopup(false);
+    }
+  };
+  
+  //error.msg='Такого аккаунта не существует';
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
@@ -11,11 +29,11 @@ function Main() {
         <p className={styles.subtitle}>Введите ваш никнейм Telegram или адрес канала</p>
         <form className={styles.form}>
           <div className={styles.input_container}>
-            <input className={error?styles.error:styles.input} type='text' placeholder='Укажите никнейм'/>
-            {error&&<div className={styles.error_msg}>{error.message}</div>}
+            <input name='account' className={error.msg?styles.error:styles.input} type='text' placeholder='Укажите никнейм'/>
+            {error.msg&&<div className={styles.error_msg}>{error.msg}</div>}
           </div>
           
-          <button className={styles.btn}>Анализировать</button>
+          <button className={styles.btn} onClick={(e)=>handleClick(e)}>Анализировать</button>
         </form>
       </div>
       </div>
@@ -29,7 +47,18 @@ function Main() {
 
       </div>
     </div>
-    </div>
+    {popup&&<div className={styles.popup} onClick={handlePopupClick}>
+      <div className={styles.popup_content}>
+        <div className={styles.title_wrapper}>
+          <div className={styles.popup_title}>Чтобы посмотреть вашу статистику, авторизуйтесь в Telegram</div >
+          <div className={styles.exit} onClick={()=>setPopup(false)}>+</div>
+        </div>
+        <a className={styles.btn}>
+          <img src={tlg} alt="telegram icon"/> Войти в аккаунт
+        </a>
+      </div >
+    </div >}
+  </div>
   );
 }
 
