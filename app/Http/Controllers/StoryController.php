5b2d12ac8fc637fc0250d63c39f89c648f5d59f6
ava-mirtl <ava_mirtl@mail.ru>
2023-12-18 23:49:15 +0300

    object to front-end

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -19,22 +19,7 @@ class StoryController extends Controller
             ->where('subscriptions.user_id', '=', $id)
             ->whereNotNull('subscriptions.followed_by_user' )
             ->select('contacts.*')
-            ->get()
-            ->toArray();
-
-
-        $stories = DB::table('stories')
-            ->join('views', 'views.story_id', '=', 'stories.id')
-            ->where('stories.user_id', '=', $id)
-            ->join('contacts', 'contacts.id', '=', 'views.follower_id')
-            ->groupBy('views.follower_id')
-            ->get()
-            ->toArray();
-//var_dump($followers);
-//die();
-
-
-
+            ->count();
         $user[0]->premium_followers =  DB::table('contacts')
             ->join('subscriptions', 'contacts.id', '=', 'subscriptions.contact_id')
             ->where('subscriptions.user_id', '=', $id)
@@ -42,7 +27,41 @@ class StoryController extends Controller
             ->where('contacts.premium', '=', 1 )
             ->select('contacts.*')
             ->count();
+        $user[0]->auditory =
+            DB::table('contacts')
+                ->join('subscriptions', 'contacts.id', '=', 'subscriptions.contact_id')
+                ->where('subscriptions.user_id', '=', $id)
+                ->where('subscriptions.follower', '=', 1 )
+                ->select('contacts.*')
+                ->get();
+        $user[0]->followers = $user[0]->auditory->count();
+        $user[0]->activity = DB::table('activities')
+            ->where('user_id', '=', $id)
+            ->get()
+            ->toArray();
 
+        $user[0]->stories = DB::table('stories')
+            ->where('stories.user_id', '=', $id)
+            ->get()
+            ->toArray();
+
+        foreach ($user[0]->stories as &$value) {
+            $value->people = DB::table('views')
+                ->select('contacts.path', 'contacts.name', 'contacts.id', 'contacts.status', 'views.like', 'views.created_at')
+                ->where('views.story_id', '=', $value->id)
+                ->join('contacts', 'contacts.id', '=', 'views.follower_id')
+                ->get()
+                ->toArray()
+            ;
+
+            foreach ($value->people as &$person) {
+                $person->views = DB::table('views')
+                    ->where('views.user_id', '=', $id)
+                    ->where('views.follower_id', '=', $person->id)
+                    ->count()
+                ;
+            }
+        }
 
 var_dump($user[0]);
 die();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,9 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         for ($i = 0; $i < 10; $i++) {
-//            \App\Models\User::factory()->create();
-//            \App\Models\Contact::factory(40)->create();
-//            \App\Models\Story::factory(15)->create(
+            \App\Models\User::factory()->create();
+            \App\Models\Contact::factory(40)->create();
+            \App\Models\Story::factory(15)->create(
+                [
+                    'user_id' => $i+1,
+
+                ]);
             \App\Models\Activity::factory()->create(
                 [
                     'user_id' => $i+1,
@@ -21,28 +27,49 @@ class DatabaseSeeder extends Seeder
                 ]);
         };
 
-//        for ($i = 0; $i < 400; $i++) {
-//            \App\Models\Subscription::factory()->create([
-//                'user_id' => rand(1, 10),
-//                'contact_id' => rand(1, 400),
-//            ]);
-//              if ($i<150){
-//            \App\Models\View::factory()->create(
-//                [
-//                    'story_id' => $i + 1,
-//                    'follower_id' => $i + 1,
-//                ]
-//            );
+        for ($i = 0; $i < 400; $i++) {
+            \App\Models\Subscription::factory()->create([
+                'user_id' => rand(1, 10),
+                'contact_id' => rand(1, 400),
+            ]);
+              if ($i<150){
+            \App\Models\View::factory()->create(
+                [
+                    'story_id' => $i + 1,
+                    'follower_id' => $i + 1,
+                ]
+            );
+
+               }
+               else{
+                   \App\Models\View::factory()->create(
+                       [
+                           'story_id' => rand(1, 150),
+                           'follower_id' => $i + 1,
+                       ]
+                   );
+               }
+        };
+
+
 //
-//               }
-//               else{
-//                   \App\Models\View::factory()->create(
-//                       [
-//                           'story_id' => rand(1, 150),
-//                           'follower_id' => $i + 1,
-//                       ]
-//                   );
-//               }
-//        };
+//        $views = DB::table('views')->get();
+//
+//        foreach ($views as $view) {
+//            $user_id = DB::table('stories')
+//                ->where('id', $view->story_id)
+//                ->select('user_id')
+//                ->first();
+//
+//            if ($user_id) {
+//                $user_id = $user_id->user_id;
+//
+//                DB::table('views')
+//                    ->where('id', $view->id)
+//                    ->update(['user_id' => $user_id]);
+//            };
+//        }
+//
+
     }
 }
